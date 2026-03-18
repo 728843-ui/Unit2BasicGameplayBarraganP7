1e0ff7fb3e676fe3eb9bf1bbf09ee8be8a6743f2
728843-ui <728843@student.dusd.net>
2026-03-18 15:27:14 -0700

    gr3tegwrg

--- P7BasicGameplayBarragan/Assets/Scripts/SpawnManager.cs
+++ P7BasicGameplayBarragan/Assets/Scripts/SpawnManager.cs
@@ -17,10 +17,7 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnRandomAnimal();
-        }
+       
     }
 
     void SpawnRandomAnimal()
